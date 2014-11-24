f6933a82e7c238f3902745c471d7f969a6fe1c98
Mario Silveira <mario.silveira@globant.com>
2014-11-24 17:31:53 -0300

    git ignore

---
+++ Magochi/Magochi/InstanciaMascota.h
@@ -0,0 +1,13 @@
+//
+//  InstanciaMascota.h
+//  Magochi
+//
+//  Created by Mario Silveira on 11/24/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InstanciaMascota : NSObject
+
+@end
---
+++ Magochi/Magochi/InstanciaMascota.m
@@ -0,0 +1,13 @@
+//
+//  InstanciaMascota.m
+//  Magochi
+//
+//  Created by Mario Silveira on 11/24/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import "InstanciaMascota.h"
+
+@implementation InstanciaMascota
+
+@end
